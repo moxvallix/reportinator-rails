6fe5184d66de509a6cd4e66c7bf13340923ccf6c
Moxvallix <moxvallix@gmail.com>
2022-11-02 16:21:52 +1030

    feat(report): added ability to download report as csv

--- app/controllers/reportinator/rails/reports_controller.rb
+++ app/controllers/reportinator/rails/reports_controller.rb
@@ -12,24 +12,55 @@ module Reportinator::Rails
 
     # GET /reports/1
     def show
+      @template = params[:id]
+      @params = {template: @template}
+      if params[:reportinator_report].present?
+        @params.merge!({reportinator_report: params.require(:reportinator_report).permit!})
+      end
     end
 
     def update
     end
 
+    def download
+      variables = get_report_variables(params[:reportinator_report])
+      template = params[:template]
+      if template.present?
+        loader = get_loader(template, variables)
+        set_variables(loader)
+        data = @report.to_csv
+        filename = "#{template.parameterize}-#{Time.now.strftime("%Y%m%d")}.csv"
+        send_data(data, filename: filename)
+      else
+        redirect_to root_path(template), notice: "Missing template"
+      end
+    end
+
     private
       # Use callbacks to share common setup or constraints between actions.
       def set_report
-        if params[:reportinator_report].present?
-          @variables = transform_params(params[:reportinator_report])
-        end
-        @loader = Reportinator::ReportLoader.load(params[:id], variables: @variables)
-        @metadata = @loader.get_metadata
-        @report = @loader.report
-        @template = params[:id]
+        variables = get_report_variables(params[:reportinator_report])
+        loader = get_loader(params[:id], variables)
+        set_variables(loader)
       # rescue
       #   @report = Reportinator::Report.new
       #   @report.insert(Reportinator::Row.create("Invalid Table"))
+      end
+
+      def get_report_variables(inputs)
+        return unless inputs.present?
+        transform_params(inputs)
+      end
+
+      def get_loader(template, variables)
+        Reportinator::ReportLoader.load(template, variables: variables)
+      end
+
+      def set_variables(loader)
+        return false unless loader.instance_of? Reportinator::ReportLoader
+        @loader = loader
+        @metadata = loader.get_metadata
+        @report = loader.report
       end
 
       # Only allow a list of trusted parameters through.
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Reportinator::Rails::Engine.routes.draw do
   resources :reports
 
   root to: "reports#index"
-
+  
+  get "/download", to: "reports#download"
   put "/*id", to: "reports#update", as: "report_update"
   get "/*id", to: "reports#show"
 end
